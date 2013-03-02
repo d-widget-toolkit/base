bc535cc78faff8b8d20e3ee588310076cd9415e0
Sebastian Graf <SebastianGraf@t-online.de>
2013-03-02 12:57:35 +0100

    Assertion failure instead of exit
    Calling exit bogs down the whole test run in a test scenario.

--- src/java/lang/util.d
+++ src/java/lang/util.d
@@ -115,8 +115,7 @@ IDwtLogger getDwtLogger(){
 void implMissing( String file, uint line ){
     getDwtLogger().fatal( file, line, "implementation missing in file {} line {}", file, line );
     getDwtLogger().fatal( file, line, "Please create a bug report at http://www.dsource.org/projects/dwt" );
-    getDwtLogger().fatal( file, line, "exiting ..." );
-    exit(1);
+    assert(false, "Implementation missing");
 }
 
 void implMissingInTango(T = void)( String file, uint line ) {
